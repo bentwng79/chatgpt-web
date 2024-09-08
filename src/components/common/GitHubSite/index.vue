772cc77ccd5a19d8ea8051b86dc683f02e074097
Ben <mr.ng@mystudies.net>
2024-09-08 18:20:40 +0800

    Update index.vue

--- src/components/common/GitHubSite/index.vue
+++ src/components/common/GitHubSite/index.vue
@@ -8,9 +8,9 @@ const releaseVersion = import.meta.env.VITE_RELEASE_VERSION || 'v0.0.0'
     <span class="text-neutral-400">
       Powered by
     </span>
-    <a href="https://chatgpt-web.dev" target="_blank">
+    <a href="https://api.mr5ai.com" target="_blank">
       <span class="text-blue-500">
-        chatgpt-web
+        Mr.🆖 AI Hub
       </span>
     </a>
     <span v-text="`${releaseVersion}-${gitCommitSha}`" />
